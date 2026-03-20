449cee671f71eeebaf8ffbf73111d5df1376922c
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-20 16:22:44 -0300

    refactor: add the block editor config

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '7a13f4a8a1ddc08741b2');
