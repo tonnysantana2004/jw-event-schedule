a37378e74ffe8247f2f623002dd71d249843e880
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-20 17:05:36 -0300

    feat: add the custom fields to the columns of the events list

--- core/JWEventSchedule.php
+++ core/JWEventSchedule.php
@@ -18,6 +18,40 @@ class JWEventSchedule
         add_action('rest_api_init', function () use ($JWESPostType) {
             $JWESPostType->create_the_custom_fields();
         });
+
+        // TODO: use the PostType class to proccess these functionalities
+        // TODO: make the columns sortable
+
+        // Set the columns
+        add_filter('manage_event_posts_columns', function ($columns) {
+
+            $newColumns = [
+                'title' => $columns['title'],
+                'event_date' => 'Event Date',
+                'event_location' => 'Event Location',
+                'date' => $columns['date']
+            ];
+
+            return $newColumns;
+
+        });
+
+        // Populate the column values
+        add_action('manage_event_posts_custom_column', function ($column, $post_id) {
+
+            if ($column === 'event_date') {
+                $value = get_post_meta($post_id, 'event_date', true);
+                $timestamp =  strtotime($value);
+                echo date_i18n(get_option('date_format') . ' ' . get_option('time_format'), $timestamp);
+            }
+
+            if ($column === 'event_location') {
+                $value = get_post_meta($post_id, 'event_location', true);
+                echo $value ? esc_html($value) : '—';
+            }
+
+        }, 10, 2);
+
     }
 
 }
