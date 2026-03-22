0750d5209762ef138826eb4b7a03d5ad7b81ef50
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-22 17:33:44 -0300

    refactor: use functions instead of raw code to enqueue scripts

--- src/EnqueueScripts.php
+++ src/EnqueueScripts.php
@@ -6,58 +6,67 @@ class EnqueueScripts
 {
     public function init()
     {
+        $this->gutenberg();
+        $this->frontend();
+    }
+
+    public function gutenberg()
+    {
         add_action(
             'enqueue_block_editor_assets',
             function () {
 
-                wp_enqueue_script(
-                    'jwes_gutenberg_script',
-                    plugins_url('build/index.js', JWES_PLUGIN_FILE),
-                    array('wp-edit-post', 'wp-element', 'wp-components', 'wp-plugins', 'wp-data'),
-                    filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'build/index.js'),
-                    true
+                $this->enqueue_script(
+                    'build/index.js',
+                    array('wp-edit-post', 'wp-element', 'wp-components', 'wp-plugins', 'wp-data')
                 );
 
-                wp_enqueue_style(
-                    'jwes_gutenberg_style',
-                    plugins_url('assets/gutenberg/sidebar.css', JWES_PLUGIN_FILE),
-                    array(),
-                    filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'assets/gutenberg/sidebar.css'),
-                );
+                $this->enqueue_style('assets/gutenberg/sidebar.css');
             }
         );
+    }
 
+    public function frontend()
+    {
         add_action(
             'wp_enqueue_scripts',
             function () {
 
-                wp_enqueue_style(
-                    'jwes_frontend_style',
-                    plugins_url('assets/frontend/style.css', JWES_PLUGIN_FILE),
-                    array(),
-                    filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'assets/frontend/style.css')
-                );
+                if (!is_admin()) {
+                    $this->enqueue_style('assets/frontend/style.css');
+                }
 
                 if (is_singular('event')) {
-
-                    wp_enqueue_style(
-                        'jwes_frontend_single_event_style',
-                        plugins_url('assets/frontend/single-event.css', JWES_PLUGIN_FILE),
-                        array(),
-                        filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'assets/frontend/single-event.css')
-                    );
+                    $this->enqueue_style('assets/frontend/single-event.css');
                 }
 
                 if (is_post_type_archive('event') || is_tax('event_type')) {
-
-                    wp_enqueue_style(
-                        'jwes_frontend_archive_event_style',
-                        plugins_url('assets/frontend/archive-event.css', JWES_PLUGIN_FILE),
-                        array(),
-                        filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'assets/frontend/archive-event.css')
-                    );
+                    $this->enqueue_style('assets/frontend/archive-event.css');
                 }
             }
+        );
+    }
+
+    public function enqueue_style($related_file_path, $handle = false)
+    {;
+        wp_enqueue_style(
+            $handle ? '' : str_replace(['/', '.'], '-', $related_file_path),
+            plugins_url($related_file_path, JWES_PLUGIN_FILE),
+            array(),
+            filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . $related_file_path),
+            false
+        );
+    }
+
+
+    public function enqueue_script($related_file_path, $dependencies = '', $handle = false)
+    {
+        wp_enqueue_script(
+            $handle ? '' : str_replace(['/', '.'], '-', $related_file_path),
+            plugins_url($related_file_path, JWES_PLUGIN_FILE),
+            array($dependencies),
+            filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . $related_file_path),
+            true
         );
     }
 }
