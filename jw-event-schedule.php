f07ad9f694165a7a887a6813f7634b4f3067eaf6
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-22 01:34:02 -0300

    refactor: move init logic to the main class

--- jw-event-schedule.php
+++ jw-event-schedule.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:     JW Event Schedule
  * Plugin URI:      https://portfolio.tonnysantana.com
- * Description:     A plugin for create, manage and list schedules in wordpress.
+ * Description:     A plugin for create, manage and list schedules in WordPress.
  * Author:          Tonny Santana
  * Author URI:      https://portfolio.tonnysantana.com
  * Text Domain:     jw-event-schedule
@@ -12,34 +12,14 @@
  * Requires at least: 6.9.4
  * Requires PHP: 8.2
  *
- * @package         Jw_Event_Schedule
+ * @package         JW Event Schedule
  */
 
 defined('ABSPATH') || exit;
-
 defined('JWES_PLUGIN_FILE') || define('JWES_PLUGIN_FILE', __FILE__);
 defined('JWES_PLUGIN_DIR') || define('JWES_PLUGIN_DIR', __DIR__);
 
-// Load the autoload and core files
-require dirname(JWES_PLUGIN_FILE)  . '/vendor/autoload.php';
-require dirname(JWES_PLUGIN_FILE) . '/core/JWEventSchedule.php';
+// Load the autoload and core files.
+require dirname(JWES_PLUGIN_FILE) . '/vendor/autoload.php';
 
-JWEventSchedule::init();
-
-add_action('enqueue_block_editor_assets', function () {
-
-    wp_enqueue_script(
-        'jwes_plugin_script',
-        plugins_url('build/index.js', JWES_PLUGIN_FILE),
-        array( 'wp-edit-post', 'wp-element', 'wp-components', 'wp-plugins', 'wp-data' ),
-        filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'build/index.js')
-    );
-
-    wp_enqueue_style(
-        'jwes_plugin_style',
-        plugins_url('assets/gutenberg/sidebar.css', __FILE__),
-        array(),
-        filemtime(plugin_dir_path(JWES_PLUGIN_FILE) . 'assets/gutenberg/sidebar.css')
-    );
-
-});
+\JWES\Main::init();
