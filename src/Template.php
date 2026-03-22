a85677b4406866dfda8ee989567bf9dd16f20c30
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-22 07:59:42 -0300

    refactor: add a space

--- src/Template.php
+++ src/Template.php
@@ -6,7 +6,7 @@ class Template
 {
     public function singleEvent($originalTemplate)
     {
-        $singleTemplate = JWES_PLUGIN_DIR .'/templates/single-event.php';
+        $singleTemplate = JWES_PLUGIN_DIR . '/templates/single-event.php';
 
         if ('event' === get_post_type(get_the_ID())) {
             if (file_exists($singleTemplate)) {
