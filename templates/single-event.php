69b027da209efd21ab32141670a0b5128fc4a1dc
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-23 06:10:55 -0300

    refactor: add fallbacks

--- templates/single-event.php
+++ templates/single-event.php
@@ -2,72 +2,74 @@
 
 use \JWES\PostType;
 
-include('header.php'); 
+include('header.php');
 
 ?>
 
 <main>
 
 	<section class="jwes-container jwes-event-header">
-		<?php the_post_thumbnail() ?>
+		
+		<?php
+
+		if (get_the_post_thumbnail_url()) {
+			the_post_thumbnail();
+		} else { ?>
+
+			<img src="<?= plugins_url('assets/images/jwes-thumbnail.webp', JWES_PLUGIN_FILE); ?> ?>">
+
+		<?php }; ?>
 
 		<div class="jwes-event-intro">
 
 			<h1 class="jwes-title jwes-title-1"><?php the_title() ?></h1>
-			<p class="jwes-text jwes-text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque fermentum at lorem nec pretium. Vestibulum quam leo, fermentum et nisl ut, porta aliquam quam.</p>
+			<p class="jwes-text jwes-text-1"><?php $excerpt =  get_the_excerpt();
+												echo $excerpt ? $excerpt : esc_html("We're now running a handful of different meetup series in spaces around Toronto, from Etobicoke to Scarborough."); ?></p>
 
 			<div class="jwes-info-boxes">
 
+				<div class="jwes-info-box">
 
-				<?php if (get_post_meta(get_the_ID(), 'event_date', true)) : ?>
+					<div class="jwes-info-box-header">
 
-					<div class="jwes-info-box">
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="jwes-info-box-icon">
+							<path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
+						</svg>
 
-						<div class="jwes-info-box-header">
-
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="jwes-info-box-icon">
-								<path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clip-rule="evenodd" />
-							</svg>
-
-							<h3 class="jwes-title jwes-title-3">Event Date</h3>
-
-						</div>
-
-						<div class="jwes-info-box-description jwes-text jwes-text-2">
-							<span>
-								<?= PostType::get_the_event_date_formated(get_the_ID(), true, false); ?>
-							</span>
-							<span><?= PostType::get_the_event_date_formated(get_the_ID(), false, true); ?></span>
-						</div>
+						<h3 class="jwes-title jwes-title-3">Event Date</h3>
 
 					</div>
 
-				<?php endif; ?>
+					<div class="jwes-info-box-description jwes-text jwes-text-2">
+						<span>
+							<?= PostType::get_the_event_date_formated(get_the_ID(), true, false); ?>
+						</span>
+						<span><?= PostType::get_the_event_date_formated(get_the_ID(), false, true); ?></span>
+					</div>
 
-				<?php if (get_post_meta(get_the_ID(), 'event_location', true)) : ?>
+				</div>
 
-					<div class="jwes-info-box">
+				<div class="jwes-info-box">
 
-						<div class="jwes-info-box-header">
+					<div class="jwes-info-box-header">
 
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="jwes-info-box-icon">
-								<path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
-							</svg>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="jwes-info-box-icon">
+							<path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
+						</svg>
 
 
-							<h3 class="jwes-title jwes-title-3">The location</h3>
-
-						</div>
-
-						<div class="jwes-info-box-description jwes-text jwes-text-2">
-							<span>
-								<?= PostType::get_the_event_location_formated(get_the_ID()); ?>
-							</span>
-						</div>
+						<h3 class="jwes-title jwes-title-3">The location</h3>
 
 					</div>
 
-				<?php endif; ?>
+					<div class="jwes-info-box-description jwes-text jwes-text-2">
+						<span>
+							<?= PostType::get_the_event_location_formated(get_the_ID()); ?>
+						</span>
+					</div>
+
+				</div>
+
 
 			</div>
 
@@ -78,7 +80,14 @@ include('header.php');
 
 	<section class="jwes-container jwes-text jwes-text-1">
 		<div class="jwes-content">
-			<?php the_content() ?>
+			<?php
+
+			$content =  get_the_content();
+
+			echo $content ? $content : esc_html("We're now running a handful of different meetup series in spaces around Toronto, from Etobicoke to Scarborough.");
+
+			?>
+
 		</div>
 	</section>
 
