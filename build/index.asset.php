2be98c8faa21014349bbb746637bbdef8c7b4b87
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-23 02:13:50 -0300

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => '7599f3fa30b09189dfb1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-i18n', 'wp-plugins'), 'version' => 'f5f542932e908953294a');
