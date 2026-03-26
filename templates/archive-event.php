321b85d5875a7d5f60ce9123ad7facbe9dfbf981
Tonny Santana <tonnysantanasantos@gmail.com>
2026-03-26 07:35:43 -0300

    refactor: adapt the code sintax to met the wordpress coding standards

--- templates/archive-event.php
+++ templates/archive-event.php
@@ -20,11 +20,35 @@ wp_head();
 		<!-- 4. Search and Filtering Functionality -->
 		<form method="get" class="jwes-form">
 			
+			<?php wp_nonce_field( 'search_action', 'search_nonce' ); ?>
+
 			<div class="jwes-field-control jwes-text jwes-text-3">
 
 				<label for="search-by-title"><?php echo esc_html__( 'Search by title', 'jw-event-schedule' ); ?></label>
 
-				<input type="text" id="search-by-title" name="s" placeholder="<?php echo esc_html__( 'Start typing...', 'jw-event-schedule' ); ?>" value="<?php echo isset( $_GET['s'] ) ? $_GET['s'] : ''; ?>">
+				<?php
+
+				if (
+				isset( $_GET['search_nonce'] ) &&
+				wp_verify_nonce(
+					sanitize_text_field( wp_unslash( $_GET['search_nonce'] ) ),
+					'search_action'
+				)
+				) {
+					if ( isset( $_GET['s'] ) ) {
+						$jwes_search_value = sanitize_text_field( wp_unslash( $_GET['s'] ) );
+					}
+
+					if ( isset( $_GET['date_range'] ) ) {
+						$jwes_date_range = sanitize_text_field( wp_unslash( $_GET['date_range'] ) );
+					}
+					if ( isset( $_GET['event_type'] ) ) {
+						$jwes_event_type = sanitize_text_field( wp_unslash( $_GET['event_type'] ) );
+					}
+				}
+				?>
+
+				<input type="text" id="search-by-title" name="s" placeholder="<?php echo esc_html__( 'Start typing...', 'jw-event-schedule' ); ?>" value="<?php echo esc_attr( $jwes_search_value ); ?>">
 
 			</div>
 
@@ -37,8 +61,8 @@ wp_head();
 
 					<?php
 
-					// Optmizing the term query: get only the needed fields
-					$terms = get_terms(
+					// Optmizing the term query.
+					$jwes_terms = get_terms(
 						array(
 							'taxonomy'   => 'event_type',
 							'hide_empty' => false,
@@ -46,37 +70,37 @@ wp_head();
 						)
 					);
 
-					$terms = array_map(
+					$jwes_terms = array_map(
 						fn( $id ) => array(
 							'id'   => $id,
 							'slug' => get_term_field( 'slug', $id ),
 							'name' => get_term_field( 'name', $id ),
 						),
-						$terms
+						$jwes_terms
 					);
 
-					foreach ( $terms as $term ) :
+					foreach ( $jwes_terms as $jwes_term ) :
 						?>
 
 						<option
-							value="<?php echo $term['slug']; ?>"
+							value="<?php echo esc_attr( $jwes_term['slug'] ); ?>"
 
-							<?php
-							$selected = false;
+								<?php
+								$jwes_term_selected = false;
 
-							if ( isset( $_GET['event_type'] ) && $_GET['event_type'] === $term['slug'] ) {
-								$selected = true;
-							}
+								if ( $jwes_event_type === $jwes_term['slug'] ) {
+									$jwes_term_selected = true;
+								}
 
-							if ( is_tax( 'event_type', $term['slug'] ) ) {
-								$selected = true;
-							}
+								if ( is_tax( 'event_type', $jwes_term['slug'] ) ) {
+									$jwes_term_selected = true;
+								}
 
-							echo $selected ? 'selected' : '';
-							?>
+								echo $jwes_term_selected ? 'selected' : '';
+								?>
 							>
 
-							<?php echo esc_html__( $term['name'], 'jw-event-schedule' ); ?>
+							<?php echo esc_html( $jwes_term['name'] ); ?>
 
 						</option>
 
@@ -86,7 +110,7 @@ wp_head();
 
 			<div class="jwes-field-control jwes-text jwes-text-3" style="flex-grow:1">
 				<label for="date-range"><?php echo esc_html__( 'Date Range', 'jw-event-schedule' ); ?></label>
-				<input type="text" id="date-range" name="date_range" value="<?php echo isset( $_GET['date_range'] ) ? $_GET['date_range'] : ''; ?>" placeholder="<?php echo esc_html__( 'Select a date', 'jw-event-schedule' ); ?>">
+				<input type="text" id="date-range" name="date_range" value="<?php echo esc_attr( $jwes_date_range ); ?>" placeholder="<?php echo esc_html__( 'Select a date', 'jw-event-schedule' ); ?>">
 			</div>
 
 			<a href="/events">
@@ -106,29 +130,34 @@ wp_head();
 			while ( have_posts() ) :
 				the_post();
 				?>
-				<a class="jwes-listing-item" href="<?php echo the_permalink(); ?>">
+				<a class="jwes-listing-item" href="<?php echo esc_url( the_permalink() ); ?>">
 
 					<?php
 
+					// Post Thumbnail.
 					if ( get_the_post_thumbnail_url() ) {
-						the_post_thumbnail();
+						$jwes_img_tag = get_the_post_thumbnail();
 					} else {
-						?>
+						$jwes_post_thumbnail = plugins_url( 'build/images/jwes-thumbnail.webp', JWES_PLUGIN_FILE );
+						$jwes_img_tag        = "<img src='$jwes_post_thumbnail'>";
+					}
 
-						<img src="<?php echo plugins_url( 'build/images/jwes-thumbnail.webp', JWES_PLUGIN_FILE ); ?> ?>">
+					echo wp_kses_post( $jwes_img_tag );
 
-					<?php } ?>
+					?>
 
 					<h3 class="jwes-title jwes-title-4 jwes-line-limit-1">
-						<?php echo the_title(); ?>
+						<?php echo esc_html( the_title() ); ?>
 					</h3>
 
 					<div class="jwes-text jwes-text-3 jwes-line-limit-3 ">
 						<?php
 
-						$excerpt = get_the_excerpt();
+						$jwes_excerpt = get_the_excerpt();
 
-						echo $excerpt ? $excerpt : esc_html( "We're now running a handful of different meetup series in spaces around Toronto, from Etobicoke to Scarborough." );
+						$jwes_excerpt = $jwes_excerpt ? $jwes_excerpt : __( "We're now running a handful of different meetup series in spaces around Toronto, from Etobicoke to Scarborough.", 'jw-event-schedule' );
+
+						echo wp_kses_post( $jwes_excerpt );
 
 						?>
 
@@ -136,11 +165,11 @@ wp_head();
 
 					<div class="jwes-info-box-description jwes-text jwes-text-3">
 						<span>
-							<?php echo PostType::get_event_date( get_the_ID(), true, false ); ?>
+							<?php echo esc_html( PostType::get_event_date( get_the_ID(), true, false ) ); ?>
 
 						</span>
 						<span>
-							<?php echo PostType::get_event_location( get_the_ID() ); ?>
+							<?php echo esc_html( PostType::get_event_date( get_the_ID(), false, true ) ); ?>
 						</span>
 					</div>
 
